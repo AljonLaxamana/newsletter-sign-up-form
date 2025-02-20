a92e7ce624772824c9dd13b5afa79b2f7694e854
Aljon Laxamana <laxamana.aljon.c@gmail.com>
2025-02-20 19:37:11 +0800

    Done all features and responsiveness

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const formEl = document.querySelector("#sign-up-form");
 const emailInputEl = document.querySelector("#email");
 const emailErrorMessageEl = document.querySelector(".email-error-message");
+const alertSuccessEl = document.querySelector(".alert-success");
+const emailTextEl = document.querySelector(".email-text");
+const btnDismissEl = document.querySelector(".alert-success-btn");
 
 function validateEmail(email) {
   // Regular expressiong for basic email validation
@@ -12,17 +15,28 @@ formEl.addEventListener("submit", (e) => {
   // Prevent default bahavior
   e.preventDefault();
 
-  if (emailInputEl.value == "") {
+  emailInputEl.classList.remove("error-input");
+  emailErrorMessageEl.classList.remove("show");
+
+  if (emailInputEl.value === "") {
     emailInputEl.classList.add("error-input");
-    emailErrorMessageEl.classList.toggle("show");
+    emailErrorMessageEl.classList.add("show");
     emailErrorMessageEl.textContent = "No email entered";
   } else if (!validateEmail(emailInputEl.value)) {
     emailInputEl.classList.add("error-input");
-    emailErrorMessageEl.classList.toggle("show");
+    emailErrorMessageEl.classList.add("show");
     emailErrorMessageEl.textContent = "Valid email required";
   } else {
-    emailInputEl.classList.remove("error-input");
-    emailErrorMessageEl.classList.remove("show");
-    console.log("Success");
+    alertSuccessEl.classList.remove("hide");
+    alertSuccessEl.classList.add("show");
+    emailTextEl.textContent = emailInputEl.value;
+    formEl.classList.add("hide");
   }
+});
+
+btnDismissEl.addEventListener("click", () => {
+  alertSuccessEl.classList.remove("show");
+  alertSuccessEl.classList.add("hide");
+  emailInputEl.value = "";
+  formEl.classList.remove("hide");
 });
